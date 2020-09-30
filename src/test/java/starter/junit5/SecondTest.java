58ccf6ce9c2bd23646e9315ffeef13bad69fb91f
Liviu Carausu <liviu.carausu@gmail.com>
2020-10-01 00:52:58 +0200

    first JUnit5 tests

--- src/test/java/starter/junit5/SecondTest.java
+++ src/test/java/starter/junit5/SecondTest.java
@@ -10,15 +10,21 @@
 
 package starter.junit5;
 
+import net.thucydides.core.annotations.Steps;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import starter.steps.MathWizSteps;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Tag("fast")
 class SecondTest {
+
+	@Steps
+	MathWizSteps michael;
+
 
 	@Test
 	@DisplayName("My 2nd JUnit 5 test! 😎")
@@ -26,6 +32,16 @@ class SecondTest {
 		Calculator calculator = new Calculator();
 		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
 		assertEquals("My 2nd JUnit 5 test! 😎", testInfo.getDisplayName(), () -> "TestInfo is injected correctly");
+
+		// Given
+		michael.startsWith(1);
+
+		// When
+		michael.adds(3);
+
+		// Then
+		michael.shouldHave(3);
+
 	}
 
 }
